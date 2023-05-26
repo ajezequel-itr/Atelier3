0fbc06c5ae1851ce10d96fd3b80901505c68d3b1
Thibault Berthet <91309781+Thibaultbtt@users.noreply.github.com>
2023-05-26 18:09:27 +0200

    Delete microUser directory

--- microUser/src/main/java/com/sp/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sp.repository;
-import org.springframework.data.repository.CrudRepository;
-import com.sp.model.UserDB;
-
-public interface UserRepository extends CrudRepository<UserDB, Integer>{
-	public UserDB findByUsername(String username);
-}
